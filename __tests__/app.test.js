a30510c6bfff6fc16868d4182b0e326e94f5652f
Connor McCue <connor.mccue@comcast.net>
2022-11-27 23:16:30 -0800

    test 3 DELETE controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
@@ -28,7 +29,7 @@ describe('backend-express-template routes', () => {
     });
   });
 
-  it('POST /users/session signs in a user', async () => {
+  it('POST /users/sessions signs in a user', async () => {
     await request(app).post('/api/v1/users').send(mockUser);
     const res = await request(app).post('/api/v1/users/sessions').send({
       email: 'test@example.com',
@@ -36,6 +37,26 @@ describe('backend-express-template routes', () => {
     });
     expect(res.status).toEqual(200);
   });
+
+  it('DELETE /users/sessions signs out a user', async () => {
+    await UserService.create({ ...mockUser });
+    const agent = request.agent(app);
+    await agent.post('/api/v1/users/sessions').send({
+      email: 'test@example.com',
+      password: '12345',
+    });
+    const res = await agent.delete('/api/v1/users/sessions');
+    expect(res.status).toBe(200);
+    expect(res.body).toMatchInlineSnapshot(`
+    Object {
+      "message": "signed out"
+      "success": true,
+    }`);
+  });
+
+
+
+
   afterAll(() => {
     pool.end();
   });
